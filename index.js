bda5e1085b5bf33ae9b96e5f839bc534b7fe4f11
MadhuKumarGowda <madhukumar.gowda@gmail.com>
2024-02-15 23:32:49 +0530

    Global Error Handling Implementation

--- index.js
+++ index.js
@@ -23,4 +23,14 @@ app.all('*',(req,res,next)=>{
   })
 })
 
+// Global Error handling middlewear
+app.use((error, req,res,next)=>{
+  error.statuscode = error.statuscode || 500;
+  error.status = error.status || 'error';
+  res.status(error.statuscode).json({
+    status: error.statuscode,
+    message: error.message
+  })
+})
+
 module.exports = app;
